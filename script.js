dd4790ee21107f29629cae554c1ac6ffda4f35e2
dobrqka <dobrin.andreev@gmail.com>
2023-08-03 16:00:53 +0300

    Add display button for book cards
    Display button loops through the library array and displays
    cards containing information about the books

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let myLibrary = [];
+// array with all the book objects
 
 function Book(name) {
   // book constructor
@@ -6,19 +7,16 @@ function Book(name) {
 }
 
 function addBookToLibrary(name) {
-  // form submit will use this function to create a new  object with the given input
+  // submit button will use this function to create a new  object with the given input
   // and add it to the array
   let newBook = new Book(name);
   myLibrary.push(newBook);
 }
 
-// addBookToLibrary("The Hobbit");
-// console.log(myLibrary);
+const submitButton = document.querySelector("#submit-button");
 
 // submit button calls addBookToLibrary and passes arguments to addBookToLibrary
 // based on the current text in the form inputs
-
-const submitButton = document.querySelector("#submit-button");
 
 submitButton.addEventListener("click", (e) => {
   e.preventDefault();
@@ -26,4 +24,19 @@ submitButton.addEventListener("click", (e) => {
   addBookToLibrary(text);
   document.querySelector("form").reset();
   console.log(myLibrary);
+});
+
+// button that displays all the books in the library as cards
+
+const displayButton = document.querySelector("#display-button");
+const cardArea = document.querySelector("#card-area");
+
+displayButton.addEventListener("click", () => {
+  for (i = 0; i < myLibrary.length; i++) {
+    const bookCard = document.createElement("div");
+    bookCard.textContent = myLibrary[i].name;
+    // bookCard.textContent = "Test";
+    document.body.appendChild(bookCard);
+  }
+  console.log("display button clicked");
 });
