24035db9830aca38c9779ec045e6f9d48d08ce77
dobrqka <dobrin.andreev@gmail.com>
2023-08-04 12:42:10 +0300

    Replace display button with inbuilt function
    Function that displays all the book objects in the library array
    is no longer triggered by the display button but is rather invoked
    once the submit button is clicked and after new book object is created
    and added to the library array.

--- script.js
+++ script.js
@@ -15,24 +15,9 @@ function addBookToLibrary(name) {
 
 const submitButton = document.querySelector("#submit-button");
 
-// submit button calls addBookToLibrary and passes arguments to addBookToLibrary
-// based on the current text in the form inputs
-
-submitButton.addEventListener("click", (e) => {
-  e.preventDefault();
-  let text = document.querySelector("#book-name").value;
-  addBookToLibrary(text);
-  document.querySelector("form").reset();
-  document.querySelector("form").style.display = "none";
-  console.log(myLibrary);
-});
-
-// button that displays all the books in the library as cards
-
-const displayButton = document.querySelector("#display-button");
 const cardArea = document.querySelector("#card-area");
-
-displayButton.addEventListener("click", () => {
+// function that displays books once they are added
+const displayBooks = () => {
   while (cardArea.firstChild) {
     cardArea.removeChild(cardArea.firstChild);
   }
@@ -42,8 +27,37 @@ displayButton.addEventListener("click", () => {
     // bookCard.classList.add("card");
     cardArea.appendChild(bookCard);
   }
-  console.log("display button clicked");
+};
+
+// submit button calls addBookToLibrary and passes arguments to addBookToLibrary
+// based on the current text in the form inputs
+
+submitButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  let text = document.querySelector("#book-name").value;
+  addBookToLibrary(text);
+  document.querySelector("form").reset();
+  document.querySelector("form").style.display = "none";
+  displayBooks();
+  console.log(myLibrary);
 });
+
+// button that displays all the books in the library as cards
+
+// const displayButton = document.querySelector("#display-button");
+
+// displayButton.addEventListener("click", () => {
+//   while (cardArea.firstChild) {
+//     cardArea.removeChild(cardArea.firstChild);
+//   }
+//   for (i = 0; i < myLibrary.length; i++) {
+//     const bookCard = document.createElement("div");
+//     bookCard.textContent = myLibrary[i].name;
+//     // bookCard.classList.add("card");
+//     cardArea.appendChild(bookCard);
+//   }
+//   console.log("display button clicked");
+// });
 
 const addButton = document.querySelector("#new-book");
 // makes input form appear so you can add a new book
